f98843371e0e7531f4c942a09bba85dadc66c929
samuel <Skboakye002@st.ug.edu.gh>
2021-12-16 20:18:12 +0000

    All files download and run index

---
+++ index.js
@@ -0,0 +1,57 @@
+ 
+ function randomNum(){
+    let randomNumber = Math.floor(Math.random()*8) + 1
+    return randomNumber
+ } 
+
+function generateNumber(){
+    renderNumber()
+}
+
+
+function renderNumber(){
+     
+    firstnumber = randomNum()
+    secondnumber = randomNum()
+    thirdnumber = randomNum()
+    fourthnumber = randomNum()
+
+    if(secondnumber === firstnumber){
+        secondnumber =  randomNum() 
+        renderNumber()  
+    }
+
+    if(thirdnumber=== secondnumber || thirdnumber === firstnumber  ){
+       thirdnumber = randomNum() 
+       renderNumber() 
+    }
+
+    if (fourthnumber === thirdnumber  || fourthnumber === secondnumber || fourthnumber === firstnumber){
+        fourthnumber = randomNum()  
+        renderNumber()
+    }
+     
+
+     let pin = firstnumber + " " + secondnumber + " " + thirdnumber + " " + fourthnumber
+     document.getElementById("pin-el").textContent = pin
+
+
+    
+       
+}
+
+function copyText() {
+   
+    let inputelement = document.createElement("input")
+    inputelement.type = "text"
+    let getText = document.getElementById("pin-el").textContent
+    inputelement.value = getText
+    document.body.appendChild(inputelement)
+    inputelement.select()
+    document.execCommand('copy')
+    document.body.removeChild(inputelement)
+
+
+    alert("copied!")
+    
+  }
